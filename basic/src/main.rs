c725c061254bc4be4f0d8513f54c6f5fd8c64f88
joel-briones-B-Low <joellbriones701@gmail.com>
2026-04-03 22:41:11 -0600

    Fix variable naming in main function and update print statements for clarity

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // declare a variable
 
-    let x = 5; // always put a semicolon at the end of the line
+    let _x = 5; // always put a semicolon at the end of the line
     // all variables are inmutable by defeault
     //x = 6; // this will cause an error because x is inmutable
 
@@ -10,7 +10,7 @@ fn main() {
      */
     let mut y = 6;
 
-    println!("Hello, world! {}", x); // this functions requires a string as a arguments to print in the console
+    println!("Hello, world! {}", _x); // this functions requires a string as a arguments to print in the console
 
     println!("variable mutable before: {}", y);
     y += 5;
@@ -24,4 +24,14 @@ fn main() {
         another thing is only can be a value in concrete,
         not a result of a caculation
     */
+
+    const PI: f64 = 3.1416; // recomended to use uppercase letters for constants
+
+    println!("the value of pi is: {}", PI);
+
+    // difference
+    let x = 5; // this is a variable
+    let x = 'y'; // this is a same variable but with a different value
+
+    println!("the value of x is: {}", x);
 }
