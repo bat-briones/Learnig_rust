456146ad010217b0d8f60067ac87307367e26163
joel-briones-B-Low <joellbriones701@gmail.com>
2026-04-04 18:26:09 -0600

    Add detailed comments explaining variable types and integer sizes in main function

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+    types of variables:
+    scalar types: rust has 4 types of scalar types: integer,
+    floating-point, boolean and character
+    this types are the tradicional types of variables in programming languages
+      */
+
+    //integers types
+    // rust has different types of integgers, with sign and without sign, and with different sizes
+    // the size of the integer is in bits
+    // without sign we can save 2^(n-1) (n is the value of the size in bits)
+    // with sign we can save 2^(n-1) - 1 (n is the value of the size in bits)
 }
