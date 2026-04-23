86b27a68728f285e2796d7959cfb9e9105871e59
joel-briones-B-Low <joellbriones701@gmail.com>
2026-04-23 13:11:32 -0600

    Refactor main function in main.rs and add day5.rs with ownership and string manipulation examples

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,25 +1,35 @@
 fn main() {
-    let mensaje = "hola mundo".to_string(); // this is a string literal that we can convert to a String type using the to_string() method
+    let mensaje = "hola mundo".to_string();
+    let slice = &mensaje[5..].to_string(); // "hola "
+    println!("Mensaje completo: {}", &slice);
 
-    let y = toma_propiedad(mensaje);
-    println!("El mensaje es: {}", y);
-    //println!("El mensaje es: {}", mensaje); // this will cause a compile-time error because the ownership of the string has been moved to the toma_propiedad function
-
-    let mensaje2 = "hola mundo".to_string();
-    let (mensaje_calculado, longitud) = calcular_cadena(mensaje2);
-    println!(
-        "El mensaje calculado es: {}, y su longitud es: {}",
-        mensaje_calculado, longitud
-    );
-    //println!("El mensaje calculado es: {}, y su longitud es:
+    primera_palabra(&mensaje);
+    primera_palabra(&slice);
 }
 
-fn toma_propiedad(cadena: String) -> String {
-    println!("La cadena es: {}", cadena);
-    cadena
-}
+/*
+!IMPORTANT: La RAM al Desnudo (String vs Bytes)
 
-fn calcular_cadena(cadena: String) -> (String, usize) {
-    let size = cadena.len();
-    (cadena, size)
+Texto:   "H  o  l  a     🦀"
+chars():  1  2  3  4  5   6    (La CPU trabaja para agruparlos)
+bytes(): [72,111,108,97, 32, 240,159,166,128] (Lo que lee la CPU realmente)
+
+- ¿Por qué as_bytes() encuentra el espacio (' ') tan rápido?
+  Porque simplemente busca el número 32 en el arreglo.
+- ¿Qué pasa si busco el byte 32 dentro del cangrejo por error?
+  UTF-8 es tan brillante que garantiza que ningún byte de un emoji
+  jamás será igual a 32. Es matemáticamente seguro.
+*/
+fn primera_palabra(cadena: &String) {
+    let bytes = cadena.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            // b' ' es la forma de escribir el byte para el espacio
+            println!("Primera palabra termina en el índice: {}", i);
+            break;
+        } else {
+            println!("Byte actual: {}", item);
+        }
+    }
 }
