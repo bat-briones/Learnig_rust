200cb262cb53c6193779f741aef6dd0e76f73bdf
joel-briones-B-Low <joellbriones701@gmail.com>
2026-04-24 12:21:30 -0600

    Implement User struct with print_data method and update main function to demonstrate usage

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -3,4 +3,24 @@ struct User {
     age: u8, // 0..=255 you cant have more than 255 years old
 }
 
-fn main() {}
+impl User {
+    fn print_data(&self, date: String) {
+        println!(
+            "{} is {} years old and the date is {}",
+            self.name, self.age, date
+        );
+    }
+}
+
+/*
+methods: the methods are functions that are associated with a struct,
+they are defined inside an impl block, and they can be called on an instance of the struct, for example:
+*/
+
+fn main() {
+    let my_user = User {
+        name: String::from("John"),
+        age: 30,
+    };
+    my_user.print_data("skjda".to_string());
+}
