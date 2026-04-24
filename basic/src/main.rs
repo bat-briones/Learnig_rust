bc4a112d6f973145246e8e5f415fbb44c874023c
joel-briones-B-Low <joellbriones701@gmail.com>
2026-04-24 12:52:12 -0600

    Implement Question struct and options for true/false answers in main function

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,1 +1,43 @@
-fn main() {}
+fn main() {
+    let my_question = Question {
+        question: String::from("Is Rust a good language?"),
+        answer: Option::TrueOption,
+    };
+
+    println!("Question: {}", my_question.question);
+    println!("Answer: {}", my_question.answer.is_true_let());
+
+    let another_question = Question {
+        question: String::from("Is Rust a bad language?"),
+        answer: Option::FalseOption,
+    };
+    println!("Question: {}", another_question.question);
+    println!("Answer: {}", another_question.answer.is_false_match());
+}
+
+enum Option {
+    TrueOption,
+    FalseOption,
+}
+
+struct Question {
+    question: String,
+    answer: Option,
+}
+
+impl Option {
+    fn is_true_let(&self) -> bool {
+        if let Option::TrueOption = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn is_false_match(&self) -> bool {
+        match self {
+            Option::FalseOption => false,
+            _ => true,
+        }
+    }
+}
