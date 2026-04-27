bfb653c048f0c605c40915e333a83507fe1e26ba
joel-briones-B-Low <joellbriones701@gmail.com>
2026-04-27 12:47:48 -0600

    Remove commented-out code from main function and add day11.rs with if let example

---
+++ basic/src/day11.rs
@@ -0,0 +1,20 @@
+fn main() {
+    /*
+    the if let is way of check some cases, the main difference with
+    match is match need to check all cases, but if let only check one case, the rest of cases are ignored
+
+    also exist the natural if, this is so different, because,
+    the normal if is to ckeck yes or no to achieve a goal, but you can not access the value of the variable.
+    in if let you can access the value of the variable and do not check yes o r no, this check the pattern of the variable,
+    and if the pattern is correct, you can access the value of the variable, but if the pattern is not correct, the code inside
+    the if let will not be executed.
+     */
+
+    let x = Some(5);
+
+    if let Some(value) = x {
+        println!("The value is: {}", value);
+    } else {
+        println!("The value is None");
+    }
+}
--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,20 +1,27 @@
 fn main() {
-    /*
-    the if let is way of check some cases, the main difference with
-    match is match need to check all cases, but if let only check one case, the rest of cases are ignored
+    let mut value: Vec<i32> = Vec::new();
 
-    also exist the natural if, this is so different, because,
-    the normal if is to ckeck yes or no to achieve a goal, but you can not access the value of the variable.
-    in if let you can access the value of the variable and do not check yes o r no, this check the pattern of the variable,
-    and if the pattern is correct, you can access the value of the variable, but if the pattern is not correct, the code inside
-    the if let will not be executed.
-     */
+    value.push(32);
+    value.push(33);
 
-    let x = Some(5);
-
-    if let Some(value) = x {
-        println!("The value is: {}", value);
-    } else {
-        println!("The value is None");
+    for i in &value {
+        println!("The value is: {}", i);
     }
+
+    let mut count = 0;
+
+    loop {
+        if count >= value.len() {
+            break;
+        }
+        println!("The value is: {}", value[count]);
+        count += 1;
+    }
+    count = 0;
+    while count < value.len() {
+        println!("The value is: {}", value[count]);
+        count += 1;
+    }
+
+    println!("The value is: {:?}", &value);
 }
